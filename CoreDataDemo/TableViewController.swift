37938e7fa7c0a6f4c66adaa828b16169b93031a3
DmitryYatsyuk-dv <yatsyuk.dev@mail.ru>
2020-02-12 20:04:03 +0300

    Complete first part

--- CoreDataDemo/TableViewController.swift
+++ CoreDataDemo/TableViewController.swift
@@ -56,15 +56,23 @@ class TableViewController: UITableViewController {
         
         
     }
+
+    // We get previously saved values from Core Data
+    override func viewWillAppear(_ animated: Bool) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            toDoItems = try context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
